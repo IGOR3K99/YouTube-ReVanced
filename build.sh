11e4a100a000d6c91646b0eb6a45acbbdc06f956
IGOR3K99 <55583327+IGOR3K99@users.noreply.github.com>
2024-09-04 16:23:21 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -179,8 +179,6 @@ rm -rf temp/tmp.*
 if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
 log "\nInstall [MicroG-RE](https://github.com/WSTxda/MicroG-RE/releases) for non-root YouTube and YT Music APKs"
-log "\nInstall [Obtainium](https://github.com/ImranR98/Obtainium/releases) to update non-root YouTube and YT Music APKs to the new version"
-log "\nRepository with non-root YouTube and YT Music APKs for Obtainium: https://github.com/IGOR3K99/RVX-by-IGOR3K99/releases"
 log "\nUse [zygisk-detach](https://github.com/j-hc/zygisk-detach) and [zygisk-detach-app](https://github.com/j-hc/zygisk-detach-app/releases) to detach root ReVanced YouTube and YT Music from Play Store"
 log "\n[RVX-by-IGOR3K99](https://github.com/IGOR3K99/RVX-by-IGOR3K99)\n"
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
