9bb492338fdd2fb675751265b1928f02fe237fdd
IGOR3K99 <55583327+IGOR3K99@users.noreply.github.com>
2024-09-05 05:35:23 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -180,7 +180,7 @@ if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
 log "\nInstall [MicroG-RE](https://github.com/WSTxda/MicroG-RE/releases) for non-root YouTube RVX APK"
 log "\nUse [zygisk-detach](https://github.com/j-hc/zygisk-detach) and [zygisk-detach-app](https://github.com/j-hc/zygisk-detach-app/releases) to detach root YouTube RVX from Play Store"
-log "\n[YouTube-RVX-by-IGOR3K99](https://github.com/IGOR3K99/YouTube-RVX-by-IGOR3K99)\n"
+log "\n[YouTube-RVX](https://github.com/IGOR3K99/YouTube-RVX)\n"
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 
 SKIPPED=$(cat $TEMP_DIR/skipped 2>/dev/null || :)
